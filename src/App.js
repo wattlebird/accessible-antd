c401c29524e8b01451245aaa31d8f72c3046906f
Ronnie Wang <geniusxiaoguai@gmail.com>
2019-09-06 22:08:46 +0800

    basic menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Menu from 'antd/es/menu';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {SubMenu} = Menu
+
+class App extends Component {
+  render() {
+    return (
+      <Menu mode="horizontal">
+        <SubMenu title="About">
+          <Menu.Item>Overview</Menu.Item>
+          <Menu.Item>Administration</Menu.Item>
+          <SubMenu title="Facts">
+            <Menu.Item>History</Menu.Item>
+            <Menu.Item>Current Statistics</Menu.Item>
+            <Menu.Item>Awards</Menu.Item>
+          </SubMenu>
+          <SubMenu title="Campus Tours">
+            <Menu.Item>For prospective students</Menu.Item>
+            <Menu.Item>For alumni</Menu.Item>
+            <Menu.Item>For visitors</Menu.Item>
+          </SubMenu>
+        </SubMenu>
+        <SubMenu title="Admissions">
+          <Menu.Item>Apply</Menu.Item>
+          <SubMenu title="Tuition">
+            <Menu.Item>Undergratuate</Menu.Item>
+            <Menu.Item>Graduate</Menu.Item>
+            <Menu.Item>Professional Schools</Menu.Item>
+          </SubMenu>
+          <Menu.Item>Sign Up</Menu.Item>
+          <Menu.Divider/>
+          <Menu.Item>Visit</Menu.Item>
+          <Menu.Item>Photo Tour</Menu.Item>
+          <Menu.Item>Connect</Menu.Item>
+        </SubMenu>
+        <SubMenu title="Academics">
+          <Menu.Item>Colleges &amp; Schools</Menu.Item>
+          <Menu.Item>Programs of Study</Menu.Item>
+          <Menu.Item>Honors Programs</Menu.Item>
+          <Menu.Item>Online Courses</Menu.Item>
+          <Menu.Divider/>
+          <Menu.Item>Course Explorer</Menu.Item>
+          <Menu.Item>Register for Class</Menu.Item>
+          <Menu.Item>Academic Calendar</Menu.Item>
+          <Menu.Item>Transcripts</Menu.Item>
+        </SubMenu>
+      </Menu>
+    );
+  }
 }
 
 export default App;
